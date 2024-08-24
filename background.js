69fd3d5a44e58a108e8dac1340780e83c9bb0adb
hasann009 <136752275+hasann009@users.noreply.github.com>
2024-08-23 23:10:45 -0700

    Update background.js

--- background.js
+++ background.js
@@ -1,11 +1,32 @@
-chrome.tabs.onUpdated.addListener((tabId, tab) => {
-  if (tab.url && tab.url.includes("youtube.com/watch")) {
-    const queryParameters = tab.url.split("?")[1];
-    const urlParameters = new URLSearchParams(queryParameters);
+let captureIntervalId = null;
+let selectionRect = null;
 
-    chrome.tabs.sendMessage(tabId, {
-      type: "NEW",
-      videoId: urlParameters.get("v"),
+chrome.runtime.onMessage.addListener((message) => {
+  if (message.action === 'startCapture') {
+    selectionRect = message.selectionRect;
+    if (captureIntervalId) clearInterval(captureIntervalId);
+    captureIntervalId = setInterval(() => {
+      chrome.scripting.executeScript({
+        target: { tabId: message.tabId },
+        func: () => {
+          chrome.runtime.sendMessage({ action: 'takeScreenshot' });
+        }
+      });
+    }, 2000); // Take a screenshot every 2 seconds
+  } else if (message.action === 'stopCapture') {
+    if (captureIntervalId) clearInterval(captureIntervalId);
+    captureIntervalId = null;
+  } else if (message.action === 'takeScreenshot') {
+    chrome.tabs.captureVisibleTab({ format: 'png' }, (dataUrl) => {
+      fetch(dataUrl)
+        .then(res => res.blob())
+        .then(blob => {
+          const url = URL.createObjectURL(blob);
+          chrome.downloads.download({
+            url: url,
+            filename: `screenshot_${Date.now()}.png`
+          });
+        });
     });
   }
 });
